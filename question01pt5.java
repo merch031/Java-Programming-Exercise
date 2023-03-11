0a116cf0c415b1069023a1914cf627670b0efa94
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 20:39:36 -0600

    Chapter 1 Question 1.5
    Chapter 1 Question 1.5

---
+++ question01pt5.java
@@ -0,0 +1,16 @@
+public class question01pt5 {
+    public static void main(String[] args){
+
+     //Chapter 1 Question 5
+
+     /*
+     objective:
+     (Compute expressions) Write a program that displays the result of
+      9.5 * 4.5 - 2.5 * 3 / 45.5 - 3.5
+      */
+
+      double Solution = (((9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5)));
+      System.out.println("(9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5) = " + Solution);
+
+    }
+}
