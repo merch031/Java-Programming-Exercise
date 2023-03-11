c79306eca41e9618d08f0b7fea8855d49be46035
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 19:10:23 -0600

    Chapter 1 Question 2
    Display 5 Messages

---
+++ question01pt2.java
@@ -0,0 +1,18 @@
+public class question01pt2 {
+    public static void main(String[] args) {
+
+        // Chapter 1 Question 1.2
+
+        /*
+        Objective: (Display five messages) Write a program that displays,
+        Welcome to Java five times.
+         */
+
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+
+    }
+}
