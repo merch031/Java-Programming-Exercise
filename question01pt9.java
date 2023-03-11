bd795ace763311fc0aa10f2b29f813991323f3a4
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 22:18:41 -0600

    Chapter 1 Question 1.9
    Chapter 1 Question 1.9

---
+++ question01pt9.java
@@ -0,0 +1,22 @@
+public class question01pt9 {
+    public static void main(String[] args){
+
+    //Chapter 1 Question 1.9
+
+    /*
+    objective:
+    (Area and perimeter of a rectangle)
+    Write a program that displays the area and perimeter
+    of a rectangle with a width of 4.5 and a height of 7.9
+    using the following formula: (area = width * height)
+    */
+
+    double width = 4.5;
+    double height = 7.9;
+
+    double area = width * height;
+
+    System.out.println("The area of a rectangle with a width of 4.5 and" +
+            " a height of 7.9 = " + area);
+    }
+}
