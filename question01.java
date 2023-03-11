9ed520041592915120194bdcb373bba56d5f2499
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 18:28:30 -0600

    Chapter 1 Question 1.1
    Display 3 Messages

---
+++ question01.java
@@ -0,0 +1,15 @@
+public class question01 {
+    public static void main(String[] args) {
+
+        //Chapter 1 Question 1.1
+
+        /*
+        objective: (display 3 messages) write a program that displays, Welcome to Java,
+        Welcome to Computer Science, and Programming is Fun.
+        */
+
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is Fun.");
+    }
+}
