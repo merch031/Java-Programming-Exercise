5271b4b776764cc0e92fef322ab0232cbaba86a0
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 20:46:51 -0600

    Chapter 1 Question 1.6
    Chapter 1 Question 1.6

---
+++ question1pt6.java
@@ -0,0 +1,15 @@
+public class question1pt6 {
+    public static void main(String[] args){
+
+    // Chapter 1 Question 1.6
+
+    /*
+    objective: (Summation of a series) Write a program that displays the result of
+    1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+    */
+
+    int sumOf = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + sumOf);
+
+    }
+}
