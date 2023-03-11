a6585a6c024fdef49993b19a0203c73ca9f1cb88
merch031 <124810414+merch031@users.noreply.github.com>
2023-03-10 20:06:52 -0600

    Chapter 1 Question 1.4
    Chapter 1 Question 1.4

---
+++ question01pt4.java
@@ -0,0 +1,29 @@
+public class question01pt4 {
+    public static void main(String[] args){
+
+        // Chapter 1 Question 4
+
+        /*
+        objective: (Print a table) Write a program that displays the following table:
+        a   a^2     a^3
+        1    1       1
+        2    4       8
+        3    9       27
+        4   16       64
+        */
+        System.out.println("|=======================|");
+        System.out.println("|========T=A=B=L=E======|");
+        System.out.println("|=======================|");
+        System.out.println("|  a   |   a^2   |  a^3 |");
+        System.out.println("|======|=========|======|");
+        System.out.println("|  1   |    1    |   1  |");
+        System.out.println("|======|=========|======|");
+        System.out.println("|  2   |    4    |   8  |");
+        System.out.println("|======|=========|======|");
+        System.out.println("|  3   |    9    |   27 |");
+        System.out.println("|======|=========|======|");
+        System.out.println("|  4   |    16   |   64 |");
+        System.out.println("|======|=========|======|");
+
+    }
+}
